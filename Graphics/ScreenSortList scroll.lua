4440eb8a6bf1c44fb0f764c3e16f17d4f59c7ccb
Devin J. Pohly <djpohly@gmail.com>
2013-03-24 18:47:42 -0400

    Add subtle animation to sort list

--- Graphics/ScreenSortList scroll.lua
+++ Graphics/ScreenSortList scroll.lua
@@ -4,8 +4,8 @@ local t = Def.ActorFrame {}
 
 t[#t+1] = LoadFont("Common Normal") .. {
 	Text = gc:GetName(),
-	GainFocusCommand = cmd(diffusealpha,1;zoom,1.1),
-	LoseFocusCommand = cmd(diffusealpha,0.5;zoom,0.9),
+	GainFocusCommand = cmd(stoptweening;diffusealpha,1;linear,0.125;zoom,1.1),
+	LoseFocusCommand = cmd(stoptweening;diffusealpha,0.5;linear,0.125;zoom,0.9),
 }
 
 return t
